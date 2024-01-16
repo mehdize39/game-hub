e3b6436b75550978055a5f5907693f28b65c8356
mehdi eskandary <mehdize39@gmail.com>
2024-01-16 12:04:57 +0330

    Show Spiner in Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,25 +7,7 @@ import GameCardContainer from "./GameCardContainer";
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
   const skeletons = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    ,
-    16,
-    17,
-    18,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
   ];
   return (
     <>
